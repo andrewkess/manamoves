ca261bcd344fa39f35cad9e717a05766e2f595aa
Andrew Kessinger <53582334+andrewkess@users.noreply.github.com>
2020-10-11 01:45:40 +0200

    mobile

--- src/@lekoarts/gatsby-theme-cara/components/hero.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/hero.tsx
@@ -41,8 +41,8 @@ const Hero = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
 
 
 <Carousel.Caption className="capp">
- <h1 style={{fontFamily: "Bebas Neue", fontWeight:500, fontSize:'11vw', display:'swap', letterSpacing: '0.03em', lineHeight:'0.7em'}} >LAW OF DESIRE</h1>
- <p style={{fontFamily: "Mukta", display:'swap', lineHeight:'1.2em', fontSize:'2em'}}>Fun that's as explosively bright and provocative as ever.</p>
+ <h1 style={{fontFamily: "Bebas Neue", fontWeight:500, fontSize:'26vw', display:'swap', letterSpacing: '0.03em', lineHeight:'0.75em'}} >LAW OF DESIRE</h1>
+ <p style={{fontFamily: "Mukta", display:'swap', lineHeight:'1.2em', fontSize:'3.3vh'}}>Fun that's as explosively bright and provocative as ever.</p>
 </Carousel.Caption></a>
 </Carousel.Item>
 
@@ -50,9 +50,9 @@ const Hero = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
 <Carousel.Item                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          >
 <Image src={logoTwo} className="newFill"  alt="First slide"  />
 <Carousel.Caption className="capp">
-<h1 style={{fontFamily: "Bebas Neue", fontWeight:500, fontSize:'11vw', display:'swap', letterSpacing: '0.03em', lineHeight:'0.7em'}} >PENGUIN BLOOM</h1>
+<h1 style={{fontFamily: "Bebas Neue", fontWeight:500, fontSize:'26vw', display:'swap', letterSpacing: '0.03em', lineHeight:'0.75em'}} >PENGUIN BLOOM</h1>
 
-<p style={{fontFamily: "Mukta", display:'swap', lineHeight:'1.2em', fontSize:'2em'}} className={styles2.fake}>Graceful, thought-provoking commentary on timely sociopolitical themes.</p>
+<p style={{fontFamily: "Mukta", display:'swap', lineHeight:'1.2em', fontSize:'3.3vh'}} className={styles2.fake}>Graceful, thought-provoking commentary on timely sociopolitical themes.</p>
 </Carousel.Caption>
 </Carousel.Item>
 <Carousel.Item>
@@ -60,8 +60,8 @@ const Hero = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
 
 
 <Carousel.Caption className="capp">
-<h1 style={{fontFamily: "Bebas Neue", fontWeight:500, fontSize:'11vw', display:'swap', letterSpacing: '0.03em', lineHeight:'0.7em'}} >THE NEVERENDING STORY</h1>
-<p style={{fontFamily: "Mukta", display:'swap', lineHeight:'1.2em', fontSize:'2em'}}>A magical journey about the power of a boy's imagination to save a dying land.</p>
+<h1 style={{fontFamily: "Bebas Neue", fontWeight:500, fontSize:'26vw', display:'swap', letterSpacing: '0.03em', lineHeight:'0.75em'}} >THE NEVERENDING STORY</h1>
+<p style={{fontFamily: "Mukta", display:'swap', lineHeight:'1.2em', fontSize:'3.3vh'}}>A magical journey about the power of a boy's imagination to save a dying land.</p>
 </Carousel.Caption>
 </Carousel.Item>
 </Carousel>
