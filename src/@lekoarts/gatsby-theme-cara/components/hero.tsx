240f1cb55d17451c3a0cc4331a5c81e6f09223ce
Andrew Kessinger <53582334+andrewkess@users.noreply.github.com>
2020-10-11 01:25:56 +0200

    fontsize

--- src/@lekoarts/gatsby-theme-cara/components/hero.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/hero.tsx
@@ -42,7 +42,7 @@ const Hero = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
 
 <Carousel.Caption className="capp">
  <h1 style={{fontFamily: "Bebas Neue", fontWeight:500, fontSize:'11vw', display:'swap', letterSpacing: '0.03em', lineHeight:'0.7em'}} >LAW OF DESIRE</h1>
- <p style={{fontFamily: "Mukta", display:'swap', lineHeight:'1.2em'}}>Fun that's as explosively bright and provocative as ever.</p>
+ <p style={{fontFamily: "Mukta", display:'swap', lineHeight:'1.2em', fontSize:'2em'}}>Fun that's as explosively bright and provocative as ever.</p>
 </Carousel.Caption></a>
 </Carousel.Item>
 
@@ -52,7 +52,7 @@ const Hero = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
 <Carousel.Caption className="capp">
 <h1 style={{fontFamily: "Bebas Neue", fontWeight:500, fontSize:'11vw', display:'swap', letterSpacing: '0.03em', lineHeight:'0.7em'}} >PENGUIN BLOOM</h1>
 
-<p style={{fontFamily: "Mukta", display:'swap', lineHeight:'1.2em'}} className={styles2.fake}>Graceful, thought-provoking commentary on timely sociopolitical themes.</p>
+<p style={{fontFamily: "Mukta", display:'swap', lineHeight:'1.2em', fontSize:'2em'}} className={styles2.fake}>Graceful, thought-provoking commentary on timely sociopolitical themes.</p>
 </Carousel.Caption>
 </Carousel.Item>
 <Carousel.Item>
@@ -61,7 +61,7 @@ const Hero = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
 
 <Carousel.Caption className="capp">
 <h1 style={{fontFamily: "Bebas Neue", fontWeight:500, fontSize:'11vw', display:'swap', letterSpacing: '0.03em', lineHeight:'0.7em'}} >THE NEVERENDING STORY</h1>
-<p style={{fontFamily: "Mukta", display:'swap', lineHeight:'1.2em'}}>A magical journey about the power of a boy's imagination to save a dying land.</p>
+<p style={{fontFamily: "Mukta", display:'swap', lineHeight:'1.2em', fontSize:'2em'}}>A magical journey about the power of a boy's imagination to save a dying land.</p>
 </Carousel.Caption>
 </Carousel.Item>
 </Carousel>
