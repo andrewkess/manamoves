8030f4e404fee3bdb1b872731e87d41ea44e44a7
Andrew Kessinger <53582334+andrewkess@users.noreply.github.com>
2020-10-19 20:45:36 +0200

    test

---
+++ src/@lekoarts/gatsby-theme-cara/components/project-card.tsx
@@ -0,0 +1,69 @@
+/** @jsx jsx */
+import React from "react"
+import { jsx } from "theme-ui"
+//import logo from  "../../../../static/ramy-keyframe.jpg" // Tell webpack this JS file uses this image
+import {  Image, Carousel } from 'react-bootstrap'
+
+import styles2 from '../../../../src/hero-module.css'
+
+
+type ProjectCardProps = {
+  link: string
+  title: string
+  children: React.ReactNode
+  bg: string
+  imaged: string
+}
+
+const ProjectCard = ({ link, title, children, bg, imaged }: ProjectCardProps) => (
+  <a
+    href={link}
+    target="_blank"
+    rel="noreferrer noopener"
+    sx={{
+      width: `100%`,
+      boxShadow: `lg`,
+      position: `relative`,
+      textDecoration: `none`,
+      borderRadius: `lg`,
+      px: 0,
+      py: [0, 0],
+      color: `white`,
+      background: bg || `none`,
+      transition: `all 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275) !important`,
+      "&:hover": {
+        color: `white !important`,
+        transform: `translateY(-5px)`,
+        boxShadow: `xl`,
+      },
+    }}
+  >
+
+
+
+<Carousel.Item className="projectFill">
+<Image src={imaged} className="projectFill"  alt="NEVER"  />
+<Carousel.Caption className="cappProject">
+<h1>{title}</h1>
+<p>{imaged}</p>
+</Carousel.Caption>
+</Carousel.Item>
+
+
+    <div sx={{ opacity: 0.85, textShadow: `0 2px 10px rgba(0, 0, 0, 0.3)` }} className={styles2.fake} >{children}</div>
+    <div
+      sx={{
+        textTransform: `uppercase`,
+        letterSpacing: `wide`,
+        pt: 4,
+        fontSize: [4, 5],
+        fontWeight: `medium`,
+        lineHeight: 1,
+      }}
+    >
+      {title}
+    </div>
+  </a>
+)
+
+export default ProjectCard
